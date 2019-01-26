5de0ce85f6939845719af6a74c1ff3607d7e0658
alrondere <alrondere@gmail.com>
2019-01-26 10:43:10 +0100

    root message now depends on GET year parameter

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 let express = require('express');
-
+let url = require('url');
 let app = express();
 
 
+
 app.get('/', function(req, resp){
-    resp.send("Correction erreur");
+   
+    let params = url.parse(req.url, true).query;
+    if(params.year !== undefined)
+    {
+        if(isNaN(params.year)){
+            resp.send("Vous vous trouvez dans une année bien étrange");
+        }
+        else{
+            resp.send("Bienvenue en l'an : " + params.year);
+        }
+    }
+    else
+    {
+        resp.send("Vous voilà, visiteur intemporel !");
+    }
+
+    
 });
 
 app.listen(8080);
